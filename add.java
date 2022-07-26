00c5d8ba37b423aea25855814941a2a270c93aa6
Rajiv <rajivrb2200@gmail.com>
2022-07-26 17:37:47 +0530

    added add.java file

---
+++ add.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+class add
+{
+    public static void main(String arg[])
+    {
+        int a,b,s;
+        a=45;
+        b=55;
+        s=a+b;
+        System.out.println("Sum of two number = "+s);
+    }
+}       
